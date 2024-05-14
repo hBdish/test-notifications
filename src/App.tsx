0854844e69e83357f18b7b2fd3c111ae852fe224
Maksim Doronkin <maksim3602@gmail.com>
2024-05-14 09:31:41 +0300

    🚧 Test sw

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,46 @@
 import './App.css'
-import {useEffect} from "react";
+import {useEffect, useLayoutEffect} from "react";
 
-const registerServiceWorker = async () => {
-  if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register("service-worker-test.js", {
-        scope: '/'
-      });
-      if (registration.installing) {
-        console.log("Service worker installing");
-      } else if (registration.waiting) {
-        console.log("Service worker installed");
-      } else if (registration.active) {
-        console.log("Service worker active");
-      }
-    } catch (error) {
-      console.error(`Registration failed with ${error}`);
-    }
-  }
-};
+// const registerServiceWorker = async () => {
+//   if ("serviceWorker" in navigator) {
+//     try {
+//       const registration = await navigator.serviceWorker.register("service-worker-test.js", {
+//         scope: '/'
+//       });
+//       if (registration.installing) {
+//         console.log("Service worker installing");
+//       } else if (registration.waiting) {
+//         console.log("Service worker installed");
+//       } else if (registration.active) {
+//         console.log("Service worker active");
+//       }
+//     } catch (error) {
+//       console.error(`Registration failed with ${error}`);
+//     }
+//   }
+// };
 
 function App() {
 
-  useEffect(() => {
-    registerServiceWorker();
-    // if ("serviceWorker" in navigator) {
-    //   navigator.serviceWorker
-    //     .register("service-worker.js", {
-    //       scope: "./",
-    //     })
-    //     .then(function (registration) {
-    //       let serviceWorker: ServiceWorker | undefined
-    //
-    //       if (registration.installing) {
-    //         serviceWorker = registration.installing;
-    //         document.querySelector("#kind")!.textContent = "installing";
-    //       } else if (registration.waiting) {
-    //         serviceWorker = registration.waiting;
-    //         document.querySelector("#kind")!.textContent = "waiting";
-    //       } else if (registration.active) {
-    //         serviceWorker = registration.active;
-    //         document.querySelector("#kind")!.textContent = "active";
-    //       }
-    //
-    //       console.log(serviceWorker)
-    //
-    //       if (serviceWorker) {
-    //         console.log(serviceWorker.state)
-    //         // logState(serviceWorker.state);
-    //         serviceWorker.addEventListener("statechange", function (e) {
-    //           console.log(e.target);
-    //         });
-    //       }
-    //     })
-    //     .catch(function (error) {
-    //       console.log(`
-    //         // Произошла ошибка при регистрации Service Worker.
-    //         // Файл service-worker.js может быть недоступным или содержать ошибки синтаксиса.
-    //         ${error}`)
-    //
-    //     });
-    // } else {
-    //   console.log('// Данный браузер не поддерживает Service Worker.')
-    // }
+
+
+
+  useLayoutEffect(() => {
+    if ("serviceWorker" in navigator) {
+      // Регистрация service worker-а, расположенного в корне сайта
+      // за счёт использования дефолтного scope (не указывая его)
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then(function (registration) {
+          console.log("Service worker зарегистрирован:", registration);
+        })
+        .catch(function (error) {
+          console.log("Ошибка при регистрации service worker-а:", error);
+        });
+    } else {
+      // Текущий браузер не поддерживает service worker-ы.
+      console.log("Текущий браузер не поддерживает service worker-ы");
+    }
   }, []);
 
   return (
