1550f9e3e0e1d5fed391bfb688549a1a73e7d55c
Maksim Doronkin <maksim3602@gmail.com>
2024-05-14 09:49:52 +0300

    🐛 Test

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,6 @@ import { useLayoutEffect} from "react";
 
 function App() {
 
-
-  useLayoutEffect(() => {
-    if ("serviceWorker" in navigator) {
-      // Регистрация service worker-а, расположенного в корне сайта
-      // за счёт использования дефолтного scope (не указывая его)
-      navigator.serviceWorker
-        .register("/sw.js", { scope: "./" })
-        .then(function (registration) {
-          console.log("Service worker зарегистрирован:", registration);
-        })
-        .catch(function (error) {
-          console.log("Ошибка при регистрации service worker-а:", error);
-        });
-    } else {
-      // Текущий браузер не поддерживает service worker-ы.
-      console.log("Текущий браузер не поддерживает service worker-ы");
-    }
-  }, []);
-
   return (
     <div onLoad={() => {
     }} style={{width: '100vw', height: '100vh', background: 'lavender'}}>
