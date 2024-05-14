c4ceb4ccad8242c7069cecce32be52225f9ed4d7
Maksim Doronkin <maksim3602@gmail.com>
2024-05-14 09:13:22 +0300

    🚧 Test 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,68 @@
 import './App.css'
 import {useEffect} from "react";
 
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("service-worker.js", {
+        scope: "./",
+      });
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+
 function App() {
 
   useEffect(() => {
-
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("service-worker.js", {
-          scope: "./",
-        })
-        .then(function (registration) {
-          let serviceWorker: ServiceWorker | undefined
-
-          if (registration.installing) {
-            serviceWorker = registration.installing;
-            document.querySelector("#kind")!.textContent = "installing";
-          } else if (registration.waiting) {
-            serviceWorker = registration.waiting;
-            document.querySelector("#kind")!.textContent = "waiting";
-          } else if (registration.active) {
-            serviceWorker = registration.active;
-            document.querySelector("#kind")!.textContent = "active";
-          }
-
-          console.log(serviceWorker)
-
-          if (serviceWorker) {
-            console.log(serviceWorker.state)
-            // logState(serviceWorker.state);
-            serviceWorker.addEventListener("statechange", function (e) {
-              console.log(e.target);
-            });
-          }
-        })
-        .catch(function (error) {
-          console.log(`     
-            // Произошла ошибка при регистрации Service Worker.
-            // Файл service-worker.js может быть недоступным или содержать ошибки синтаксиса.
-            ${error}`)
-
-        });
-    } else {
-      console.log('// Данный браузер не поддерживает Service Worker.')
-    }
+    registerServiceWorker();
+    // if ("serviceWorker" in navigator) {
+    //   navigator.serviceWorker
+    //     .register("service-worker.js", {
+    //       scope: "./",
+    //     })
+    //     .then(function (registration) {
+    //       let serviceWorker: ServiceWorker | undefined
+    //
+    //       if (registration.installing) {
+    //         serviceWorker = registration.installing;
+    //         document.querySelector("#kind")!.textContent = "installing";
+    //       } else if (registration.waiting) {
+    //         serviceWorker = registration.waiting;
+    //         document.querySelector("#kind")!.textContent = "waiting";
+    //       } else if (registration.active) {
+    //         serviceWorker = registration.active;
+    //         document.querySelector("#kind")!.textContent = "active";
+    //       }
+    //
+    //       console.log(serviceWorker)
+    //
+    //       if (serviceWorker) {
+    //         console.log(serviceWorker.state)
+    //         // logState(serviceWorker.state);
+    //         serviceWorker.addEventListener("statechange", function (e) {
+    //           console.log(e.target);
+    //         });
+    //       }
+    //     })
+    //     .catch(function (error) {
+    //       console.log(`
+    //         // Произошла ошибка при регистрации Service Worker.
+    //         // Файл service-worker.js может быть недоступным или содержать ошибки синтаксиса.
+    //         ${error}`)
+    //
+    //     });
+    // } else {
+    //   console.log('// Данный браузер не поддерживает Service Worker.')
+    // }
   }, []);
 
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const registerServiceWorker = async () => {
-  if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register("service-worker.js", {
-        scope: "./",
-      });
-      if (registration.installing) {
-        console.log("Service worker installing");
-      } else if (registration.waiting) {
-        console.log("Service worker installed");
-      } else if (registration.active) {
-        console.log("Service worker active");
-      }
-    } catch (error) {
-      console.error(`Registration failed with ${error}`);
-    }
-  }
-};
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -28,6 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   </React.StrictMode>,
 )
 
-registerServiceWorker();
+
 
 
