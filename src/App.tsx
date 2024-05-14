eb6a2a9ca5557b756a0b13e9f61b48cf5a5e838f
Maksim Doronkin <maksim3602@gmail.com>
2024-05-14 08:45:14 +0300

    🚧 Test SW

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onLoad={() => {
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+          .register("service-worker.js", {
+            scope: "./",
+          })
+          .then(function (registration) {
+            let serviceWorker: ServiceWorker | undefined
+
+            if (registration.installing) {
+              serviceWorker = registration.installing;
+              document.querySelector("#kind")!.textContent = "installing";
+            } else if (registration.waiting) {
+              serviceWorker = registration.waiting;
+              document.querySelector("#kind")!.textContent = "waiting";
+            } else if (registration.active) {
+              serviceWorker = registration.active;
+              document.querySelector("#kind")!.textContent = "active";
+            }
+
+            if (serviceWorker) {
+              console.log(serviceWorker.state)
+              // logState(serviceWorker.state);
+              serviceWorker.addEventListener("statechange", function (e) {
+                console.log(e.target);
+              });
+            }
+          })
+          .catch(function (error) {
+            console.log(`     
+            // Произошла ошибка при регистрации Service Worker.
+            // Файл service-worker.js может быть недоступным или содержать ошибки синтаксиса.
+            ${error}`)
+
+          });
+      } else {
+        console.log('// Данный браузер не поддерживает Service Worker.')
+      }
+    }} style={{width: '100vw', height: '100vh', background: 'lavender'}}>
+      <h1>TEST NOTIFICATIONS</h1>
+    </div>
   )
 }
 
